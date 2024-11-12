d96ccf3d824e439a165b65f7723a43824dfe9dc6
miusarname23 <oscarmauricio631@gmail.com>
2024-11-12 09:58:19 -0500

    feat: :sparkles: Return a random greeting

--- ModuleFirts/greetings/greetings.go
+++ ModuleFirts/greetings/greetings.go
@@ -3,7 +3,19 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
+
+func randomFormat() string {
+	formats := []string{
+		"Hi %v. Welcome!",
+		"Hello %v! How are you today?",
+		"Salut %v! Comment allez-vous?",
+		"Bonjour %v! Comment puis-je vous aider?",
+		"Hola %v! ¿Cómo estás?",
+	}
+	return formats[rand.Intn(len(formats))]
+}
 
 func Hello(name string) (string, error) {
 
@@ -11,6 +23,6 @@ func Hello(name string) (string, error) {
 		return "", errors.New("Name cannot be empty")
 	}
 
-	message := fmt.Sprintf("Hi %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
