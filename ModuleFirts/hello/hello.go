579912cc80b6782edf547330e231dce1e688cfe5
miusarname23 <oscarmauricio631@gmail.com>
2024-11-12 09:26:05 -0500

    feat: :sparkles: Handling errors

--- ModuleFirts/hello/hello.go
+++ ModuleFirts/hello/hello.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(greetings.Hello("Mathew"))
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
