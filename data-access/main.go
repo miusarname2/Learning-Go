9514df7348d13f1ecb8bc2f215e18cb6f0f37c4d
miusarname23 <oscarmauricio631@gmail.com>
2024-11-13 14:56:17 -0500

    feat: :sparkles: Obtain a value form database with the id

--- data-access/main.go
+++ data-access/main.go
@@ -44,7 +44,20 @@ func albumByArtist(name string) ([]Album, error) {
 	}
 
 	return albums, nil
+}
 
+func albumById(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id =?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+
+	return alb, nil
 }
 
 func main() {
@@ -80,5 +93,13 @@ func main() {
 	}
 
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumById(2)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Album found: %+v\n", alb)
 
 }
