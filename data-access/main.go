f202aa23e10901085b49c371bd77f1d34920933b
miusarname23 <oscarmauricio631@gmail.com>
2024-11-13 14:50:10 -0500

    feat: :sparkles: Data is obtained from the database

--- data-access/main.go
+++ data-access/main.go
@@ -10,6 +10,43 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  string
+}
+
+func albumByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+
+		albums = append(albums, alb)
+
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
+
+}
+
 func main() {
 	// Configuración de la conexión a la base de datos
 	ctf := mysql.Config{
@@ -35,4 +72,13 @@ func main() {
 	}
 
 	fmt.Println("Conexión establecida")
+
+	albums, err := albumByArtist("katiusha")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Albums found: %v\n", albums)
+
 }
