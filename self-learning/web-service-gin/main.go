c943948132279d3181517061811ca6ed63823ee2
miusarname23 <oscarmauricio631@gmail.com>
2024-11-14 08:16:27 -0500

    feat: :sparkles: Create the get data operation

--- self-learning/web-service-gin/main.go
+++ self-learning/web-service-gin/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float32 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Album 1", Artist: "Artist 1", Price: 10.99},
+	{ID: "2", Title: "Album 2", Artist: "Artist 2", Price: 15.99},
+	{ID: "3", Title: "Album 3", Artist: "Artist 1", Price: 8.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
 func main() {
-	fmt.Println("Starting development server..")
+
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+
 }
